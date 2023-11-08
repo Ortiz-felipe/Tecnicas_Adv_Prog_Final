181dd0d67fc93c8c1f53669c5719b2d7263f6b44
Felipe Ortiz <ortiz.jfelipe@hotmail.com>
2023-11-08 19:35:31 -0300

    Added a Token Service

---
+++ VTVApp.Api/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using VTVApp.Api.Models.DTOs.Users;
+
+namespace VTVApp.Api.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(UserDto userInfo);
+    }
+}
---
+++ VTVApp.Api/Services/TokenService.cs
@@ -0,0 +1,45 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using VTVApp.Api.Models.DTOs.Users;
+using VTVApp.Api.Services.Interfaces;
+
+namespace VTVApp.Api.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string CreateToken(UserDto userInfo)
+        {
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, userInfo.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, userInfo.Email),
+                // Add more claims as needed
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1), // Set token expiration as needed
+                SigningCredentials = credentials,
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
