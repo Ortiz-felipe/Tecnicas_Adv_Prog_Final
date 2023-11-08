da5aabc11e93cb2a44c8e0e1d1c758c7c973d85f
Felipe Ortiz <ortiz.jfelipe@hotmail.com>
2023-11-08 19:39:55 -0300

    Added a Filter for the Date component used in Appointments

---
+++ VTVApp.Api/Filters/DateParameterOperationFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace VTVApp.Api.Filters
+{
+    public class DateParameterOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters != null)
+            {
+                foreach (var parameter in operation.Parameters)
+                {
+                    if (parameter.Name == "date" && parameter.In == ParameterLocation.Path)
+                    {
+                        parameter.Schema.Type = "string";
+                        parameter.Schema.Format = "date"; // or "date-time" if you expect time as well
+                        parameter.Example = new OpenApiString(DateTime.UtcNow.ToString("yyyy-MM-dd"));
+                    }
+                }
+            }
+        }
+    }
+}
